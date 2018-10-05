8426506a1731b52c72603186fbb41e7a6efad9c9
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-10-05 18:02:13 -0300

    Add files via upload

--- ConversorTemperatura/MainForm.cs
+++ ConversorTemperatura/MainForm.cs
@@ -46,5 +46,70 @@ namespace ConversorTemperatura
         {
 
         }
+		void BoxConverterDeSelectedIndexChanged(object sender, EventArgs e)
+		{
+			
+		}
+		void BtnConverterTemperatura_Click(object sender, EventArgs e) //botão que faz calculo
+		{				
+			if(BoxConverterDe.GetItemText(BoxConverterDe.SelectedItem == 1)
+			     {
+			     	//Celsius pra Fahrenheit
+			     	double numero, resultado1;
+			     	numero = double.parse(txtNumero.Text);
+			     	resultado1 = numero * 1,8 + 32;
+			   	 	txtResultado1.Text;
+			   
+			   	   	//Celsius pra Kelvin
+			   	   	double numero, resultado2;
+			     	numero = double.parse(txtNumero.Text);
+			     	resultado2 = numero - 273,15;
+			   	 	txtResultado2.Text;
+			    }
+			
+			//Fahrenheit pra Celsius e Kelvin
+			else
+			if(BoxConverterDe.GetItemText(BoxConverterDe.SelectedItem == 2)
+			   {
+			   		//Fahrenheit pra Celsius
+			     	double numero, resultado1;
+			     	numero = double.parse(txtNumero.Text);
+			     	resultado1 = (numero - 32) / 1,8;
+			   	 	txtResultado1.Text;
+			   	   	
+			   	   	//Fahrenheit pra Kelvin
+			   	   	double numero, resultado2;
+			     	numero = double.parse(txtNumero.Text);
+			     	resultado2 = (numero + 459,67) * 5/9;
+			   	 	txtResultado2.Text;
+			   }
+
+			//Kelvin pra Celsius e Fahrenheit
+			
+			else(BoxConverterDe.GetItemText(BoxConverterDe.SelectedItem == 3)
+			   {
+			   		//Kelvin pra Celsius
+			     	double numero, resultado1;
+			     	numero = double.parse(txtNumero.Text);
+			     	resultado1 = numero -273,15;
+			   	 	txtResultado1.Text;
+			   	   	
+			   	   	//Kelvin pra Fahrenheit
+			   	   	double numero, resultado2;
+			     	numero = double.parse(txtNumero.Text);
+			     	resultado2 = (numero + 459,67);
+			   	 	txtResultado2.Text;
+			   }
+			
+			
+		}
+		void TxtNumeroTextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void BtnConverterTemperaturaClick(object sender, EventArgs e)
+		{
+	
+		}
     }
 }
